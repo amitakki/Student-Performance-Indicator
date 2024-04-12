318838af5c461b9120ab0a0bc69f5316545a4bc1
Amit Akki <akkiamit23@gmail.com>
2024-04-12 09:08:32 +0530

    Model Training component

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -84,7 +84,7 @@ class DataTransformation:
             target_feature_train_df = train_df[target_column_name]
 
             input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
-            target_feature_test_df = test_df.drop(columns=[target_column_name])
+            target_feature_test_df = test_df[target_column_name]
 
             logging.info(
                 f"Applying preprocessing object on training dataframe and testing dataframe"
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import pickle
 
 import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -15,5 +16,26 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for name, model in models.items():
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[name] = test_model_score
+        return report
+    
     except Exception as e:
         raise CustomException(e, sys)
