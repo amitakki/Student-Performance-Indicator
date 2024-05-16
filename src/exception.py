d0eba612d4d008cf9e443af17e9250277f460964
Amit Akki <akkiamit23@gmail.com>
2024-05-16 18:01:18 +0530

    Fixed bug in exception message

--- src/exception.py
+++ src/exception.py
@@ -4,7 +4,7 @@ from src.logger import logging
 def error_message_detail(error, error_message_detail:sys):
     _, _, exc_tb = error_message_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in Python script name [{0}] line number [{1}] error message".format(
+    error_message = "Error occured in Python script name [{0}] line number [{1}] error message[{2}]".format(
     file_name, exc_tb.tb_lineno, str(error)
     )
     return error_message
